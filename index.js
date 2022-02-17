494726345cbbb866bc65dbe45a8294a94069be13
Nina Vens <nina.vens@hva.nl>
2022-02-17 18:03:44 +0100

    express, routes, handlebars

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-const lodash = require("lodash");
-const camelCase = require('camelcase');
-const express = require("express");
+const lodash = require('lodash');
+const express = require('express');
 const app = express();
+const { engine } = require('express-handlebars');
 
-app.get('/', function (req, res) {
-    res.send('hello world');
+const PORT = process.env.PORT || 3000;
+// process.env.PORT
+
+app.use('/static', express.static('static'));
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', 'views');
+
+
+
+app.get('/', (req, res) => {
+    res.render('home');
 })
 
-app.listen(3000)
+app.get('/login', (req, res) => {
+    res.send('Log in');
+})
 
+app.listen(PORT, () => {
+    console.log('app running on port', PORT);
+})
 
-console.log(camelCase('camel case'));
+app.use((req, res, next) => {
+    res.status(404).render('not-found');
+})
