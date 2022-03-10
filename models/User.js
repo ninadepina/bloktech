9815ac8bc848dda618f0ca8a57237087e8148d48
Nina Vens <nina.vens@hva.nl>
2022-03-10 13:39:35 +0100

    mongoose

--- models/User.js
+++ models/User.js
@@ -5,6 +5,19 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+    type: String,
+    required: true
     }
 })
 
