53738f8974f3e37b20508bcdb70c641cad9c15fe
Nina Vens <nina.vens@hva.nl>
2022-03-11 13:14:58 +0100

    emailaddress check

--- README.md
+++ README.md
@@ -7,5 +7,16 @@ _A platform designed to help foster pets find their new home._
 ## 📝 Installation
 
 
+## 🤖 Dependencies
+The following packages are being used:
+* [BCrypt](https://github.com/kelektiv/node.bcrypt.js) - for hashing passwords.
+* [Body-parser](https://github.com/expressjs/body-parser) - to fish out data from forms.
+* [Dotenv](https://github.com/motdotla/dotenv) - to securely store credentials.
+* [Express](https://expressjs.com/en/api.html) - used for its fantastic middleware and routing.
+* [Mongoose](https://github.com/Automattic/mongoose) - ODM for MongoDB, to make life easier.
+* [MongoDB](https://github.com/mongodb/node-mongodb-native) - as database.
+* [Multer](https://github.com/expressjs/multer) - used for uploading files.
+* [NodeJS](https://nodejs.org/en/) - for backend I/O.
+
 ## 📝 License
-[![MIT License][license-shield]](https://github.com/ninadepina/bloktech/blob/main/LICENSE)
+[MIT](https://github.com/ninadepina/bloktech/blob/main/LICENSE) © Nina Vens
--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const lodash = require('lodash');
 const express = require('express');
 const app = express();
 const { engine } = require('express-handlebars');
 const bodyParser = require('body-parser');
-const sass = require('sass'); 
 const connectDB = require('./config/db')
 const User = require('./models/User')
 require('dotenv').config()
 
 connectDB();
 
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 const multer = require('multer');
 const upload = multer({ dest: './public/data/uploads/' })
@@ -39,17 +37,53 @@ app.get('/signup', (req, res) => {
 })
 
 //signup/register page
-app.post('/signup', async (req, res) => {
-    await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        confirm_password: req.body.confirm_password,
-        namePet: req.body.namePet,
-        birthday: req.body.birthday,
-        breed: req.body.breed
-    })
-    res.redirect('/signin')
+// app.post('/signup', async (req, res) => {
+//     // const password = document.querySelector('input[name="password"]');
+//     // const confirm_password = document.querySelector('input[name="confirm_password"]');
+//     await User.create({
+//         name: req.body.name,
+//         email: req.body.email,
+//         password: req.body.password,
+//         confirm_password: req.body.confirm_password,
+//         namePet: req.body.namePet,
+//         birthday: req.body.birthday,
+//         breed: req.body.breed
+//     })
+//     res.redirect('/signin')
+// });
+
+app.post('/signup', (req, res) => {
+    try {
+        User.findOne({email: req.body.email}).then((user) => {
+            if (user) {
+                //An account with this emailaddress already exists
+                return res.status(400).send("An account with this emailaddress already exists..")
+            } else {
+                //emailaddress has not been used before
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password,
+                    confirm_password: req.body.confirm_password,
+                    namePet: req.body.namePet,
+                    birthday: req.body.birthday,
+                    breed: req.body.breed
+                });
+
+                if (req.body.password === req.body.confirm_password) {
+                    console.log('Password === Confirm Password')
+                } else {
+                    console.log("Password !=== Confirm Password")
+                    res.status(400).send("Password is not the same")
+                }  
+
+                newUser.save()
+                res.redirect('/signin');
+            }
+        });
+    } catch (error) {
+        throw new Error(error);
+    }
 });
 
 //signin/login page
