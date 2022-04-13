73ffe6cce34f9b2037d2dc30b1c81a1176cc1cf9
Nina Vens <nina.vens@hva.nl>
2022-04-13 12:36:58 +0200

    [Hotfix]

--- static/scripts/formError.js
+++ static/scripts/formError.js
@@ -4,96 +4,96 @@ function isEmail(email) {
 }
 
 // check if all input meet all requirements
-const form_signup = document.querySelector('#form-signup');
+const form_signup = document.querySelector("#form-signup");
 
 if (form_signup) {
-    form_signup.addEventListener('click', (e) => {
-        const formName = document.querySelector('#name');
-        const formEmail = document.querySelector('#email');
-        const formUsername = document.querySelector('#username');
-        const formPassword = document.querySelector('#password');
-        const formConfirmPassword = document.querySelector('#confirm_password');
-        const formPetName = document.querySelector('#namePet');
-        const formBirthday = document.querySelector('#birthday');
-        const formBreed = document.querySelector('#breed');
+    form_signup.addEventListener("click", (e) => {
+        const form_name = document.querySelector("#name");
+        const form_email = document.querySelector("#email");
+        const form_username = document.querySelector("#username");
+        const form_password = document.querySelector("#password");
+        const form_confirm_password = document.querySelector("#confirm_password");
+        const form_pet_name = document.querySelector("#name_pet");
+        const form_birthday = document.querySelector("#birthday");
+        const form_breed = document.querySelector("#breed");
 
-        if (formName.value.length != 0 && isEmail(formEmail.value) && formPassword.value.length != 0 && formPassword.value === formConfirmPassword.value) {
-            console.log('All info is okay');
+        if (form_name.value.length != 0 && isEmail(form_email.value) && form_password.value.length != 0 && form_password.value === form_confirm_password.value) {
+            return
         } else {
             e.preventDefault()
 
             // check given name
-            if (formName.value.length != 0) {
-                console.log('A name has been given');
-                formName.classList.remove('error');
+            if (form_name.value.length != 0) {
+                console.log("A name has been given");
+                form_name.classList.remove("error");
             } else {
-                console.log('No name has been given');
-                formName.classList.add('error');
+                console.log("No name has been given");
+                form_name.classList.add("error");
             }
 
             // check given email
-            if (formEmail.value.length = 0) {
-                console.log('No email has been given');
-                formEmail.classList.add('error');
-            } else if (!isEmail(formEmail.value)) {
-                console.log('Given email is not valid');
-                formEmail.classList.add('error');
+            if (form_email.value.length = 0) {
+                console.log("No email has been given");
+                form_email.classList.add("error");
+            } else if (!isEmail(form_email.value)) {
+                console.log("Given email is not valid");
+                form_email.classList.add("error");
             } else {
-                console.log('Email has been given');
-                formEmail.classList.remove('error');
+                console.log("Email has been given");
+                form_email.classList.remove("error");
             }
 
             // check given username
-            if (formUsername.value.length != 0) {
-                console.log('A username has been given');
-                formUsername.classList.remove('error');
+            if (form_username.value.length != 0) {
+                console.log("A username has been given");
+                form_username.classList.remove("error");
             } else {
-                console.log('No username has been given');
-                formUsername.classList.add('error');
+                console.log("No username has been given");
+                form_username.classList.add("error");
             }
 
             // check given password
-            if (formPassword.value.length != 0) {
-                if (formPassword.value === formConfirmPassword.value) {
-                    console.log('Passwords are the same');
-                    formPassword.classList.remove('error');
-                    formConfirmPassword.classList.remove('error');
+            if (form_password.value.length != 0) {
+                if (form_password.value === form_confirm_password.value) {
+                    console.log("Passwords are the same");
+                    form_password.classList.remove("error");
+                    form_confirm_password.classList.remove("error");
                 } else {
-                    console.log('Passwords are NOT the same');
-                    formPassword.classList.add('error');
-                    formConfirmPassword.classList.add('error');
+                    console.log("Passwords are NOT the same");
+                    form_password.classList.add("error");
+                    form_confirm_password.classList.add("error");
                 }
             } else {
-                console.log('No password has been given');
-                formPassword.classList.add('error');
-                formConfirmPassword.classList.add('error');
+                console.log("No password has been given");
+                form_password.classList.add("error");
+                form_confirm_password.classList.add("error");
             }
 
             // check given petname
-            if (formPetName.value.length != 0) {
-                console.log('A petname has been given');
-                formPetName.classList.remove('error');
+            if (form_pet_name.value.length != 0) {
+                console.log("A petname has been given");
+                form_pet_name.classList.remove("error");
             } else {
-                console.log('No petname has been given');
-                formPetName.classList.add('error');
+                console.log("No petname has been given");
+                form_pet_name.classList.add("error");
             }
 
             // check given birthday
-            if (formBirthday.value.length != 0) {
-                console.log('A birthday has been given');
-                formBirthday.classList.remove('error');
+            if (form_birthday.value.length != 0) {
+                console.log("A birthday has been given");
+                form_birthday.classList.remove("error");
             } else {
-                console.log('No birthday has been given');
-                formBirthday.classList.add('error');
+                console.log("No birthday has been given");
+                form_birthday.classList.add("error");
             }
 
             // check given breed
-            if (formBreed.value.length != 0) {
-                console.log('A breed has been given');
-                formBreed.classList.remove('error');
+            if (form_breed.value.length != 0) {
+                console.log("A breed has been given");
+                form_breed.classList.remove("error");
             } else {
-                console.log('No breed has been given');
-                formBreed.classList.add('error');
+                console.log("No breed has been given");
+                form_breed.classList.add("error");
             }
         }
     });
